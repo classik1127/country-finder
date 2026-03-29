3154087fa546939fbf9e3f1583cbe0d20849cb58
victor <ve002601@gmail.com>
2026-03-29 19:27:07 +0100

    adding javascript function

--- result.js
+++ result.js
@@ -1,12 +1,11 @@
 const params = new URLSearchParams(window.location.search)
 
-const newcon = params.get("country");
+const countrysearch = params.get("country");
 
 const result = document.getElementById("resultname");
-result.textContent = newcon;
-// console.log(newcon);
+result.textContent = countrysearch;
 
-
+document.addEventListener("DOMContentLoaded", fetch)
 // for switching page back to home
 
 function switchpage() {
@@ -17,21 +16,56 @@ const notfound = document.getElementById("notfound");
 const found = document.getElementById("found");
             found.style.display = "flex";
 
-fetch("https://restcountries.com/v3.1/name/japan")
-    .then(Response => Response.json())
-    .then(data => {
-            console.log(data)
-        if (data.status === 404) {
-            notfound.style.display = "block";
-            found.style.display = "none";
-            
-        }
-        else{            
-            found.style.display = "flex: ";
-            console.log("data")
-        }
-    }
-    )
-
 
 // for page with result
+
+const continet = document.getElementById("continet");
+const countryname = document.getElementById("countryname");
+const maplink = document.getElementById("maplink");
+const capitalname = document.getElementById("capitalname");
+const Population = document.getElementById("Population");
+const Area = document.getElementById("Area");
+const Timezone = document.getElementById("Timezone");
+const Languages = document.getElementById("Languages");
+const Currencies = document.getElementById("Currencies");
+const Neighborone = document.getElementById("Neighborone");
+const Neighbortwo = document.getElementById("Neighbortwo");
+const Neighborthree = document.getElementById("Neighborthree");
+const Neighborfour = document.getElementById("Neighborfour");
+
+const countryflag = document.getElementById("countryflag");
+
+
+console.log(countrysearch);
+let newcon = countrysearch;
+    try {
+         fetch(`https://restcountries.com/v3.1/name/${newcon}`)
+            .then(Response => Response.json())
+            .then(data  => {
+                let datainfo = data[0];
+                console.log(datainfo)
+                console.log(datainfo.currencies)
+
+                
+                                countryflag.alt  = datainfo.flags.alt ;
+                                
+                                countryflag.src  = datainfo.flags.png ;
+                continet.textContent = datainfo.continents;
+                countryname.textContent = countrysearch;
+                // maplink.href = maps.googleMaps;
+                capitalname.textContent = datainfo.capital;
+                Population.textContent = datainfo.population;
+                Area.textContent = datainfo.area;
+                Area.textContent = datainfo.area;
+                Timezone.textContent = datainfo.timezones[0];
+                // Currencies.textContent = datainfo.Currencies.name;
+                Neighborone.textContent = datainfo.borders[0];
+                Neighbortwo.textContent = datainfo.borders[1];
+                Neighborthree.textContent = datainfo.borders[2];
+                Neighborfour.textContent = datainfo.borders[3];
+
+
+            })
+    } catch (error) {
+        console.error()
+    }
